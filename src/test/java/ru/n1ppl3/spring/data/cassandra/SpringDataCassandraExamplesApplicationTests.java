b6e23f891e9c1b3c9f1caf272650ea52dde9f588
n1ppl3 <n1ppl3@gmail.com>
2021-03-10 17:19:28 +0300

    embedded Cassandra in test

--- src/test/java/ru/n1ppl3/spring/data/cassandra/SpringDataCassandraExamplesApplicationTests.java
+++ src/test/java/ru/n1ppl3/spring/data/cassandra/SpringDataCassandraExamplesApplicationTests.java
@@ -1,13 +1,28 @@
 package ru.n1ppl3.spring.data.cassandra;
 
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class SpringDataCassandraExamplesApplicationTests {
 
+	@BeforeAll
+	static void beforeAll() throws IOException, InterruptedException {
+		EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+	}
+
 	@Test
 	void contextLoads() {
+	}
+
+	@AfterAll
+	static void afterAll() {
+		EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
 	}
 
 }
