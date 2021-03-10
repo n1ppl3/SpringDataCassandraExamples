4caad4dd1fdda5025266051350686eb9d87eeb94
n1ppl3 <n1ppl3@gmail.com>
2021-03-10 10:32:18 +0300

    init

---
+++ src/main/java/ru/n1ppl3/spring/data/cassandra/SpringDataCassandraExamplesApplication.java
@@ -0,0 +1,13 @@
+package ru.n1ppl3.spring.data.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataCassandraExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataCassandraExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/n1ppl3/spring/data/cassandra/SpringDataCassandraExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.n1ppl3.spring.data.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataCassandraExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
